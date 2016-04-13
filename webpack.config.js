9dfce63ec12d254b7322ee369804768ad01477f9
Robin Wieruch <wrobin@gmx.net>
2016-04-13 19:51:14 +0200

    feat(index.js): jsx to js

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,11 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.jsx'
+    './src/index.js'
   ],
   module: {
-    preLoaders: [
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      },
-    ],
     loaders: [{
-      test: /\.jsx?$/,
+      test: /\.js?$/,
       exclude: /node_modules/,
       loader: 'react-hot!babel'
     },
